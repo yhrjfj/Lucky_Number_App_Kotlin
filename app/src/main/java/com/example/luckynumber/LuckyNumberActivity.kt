034053cfcc94927f18e49e184955ac0e4c9ed0c8
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-14 21:25:06 +0600

    Declaring & Initialising

--- app/src/main/java/com/example/luckynumber/LuckyNumberActivity.kt
+++ app/src/main/java/com/example/luckynumber/LuckyNumberActivity.kt
@@ -2,10 +2,17 @@ package com.example.luckynumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class LuckyNumberActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lucky_number)
+
+        // Initializing
+        val textLNA : TextView = findViewById(R.id.textView2)
+        val luckyNumberText : TextView = findViewById(R.id.luckyNumberText)
+        val shareButton : Button = findViewById(R.id.share)
     }
 }
--- app/src/main/java/com/example/luckynumber/MainActivity.kt
+++ app/src/main/java/com/example/luckynumber/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.luckynumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initializing
+        val text : TextView = findViewById(R.id.textView)
+        val name : EditText = findViewById(R.id.editText)
+        val findLuckyNUmberButton : Button = findViewById(R.id.btn)
     }
 }
